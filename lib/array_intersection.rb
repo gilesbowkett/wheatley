9fa01af41bf0117f1d84baefffefa8bcfb1ee450
Giles Bowkett <gilesb@gmail.com>
2012-10-30 23:28:23 -0700

    removed totally useless line of code
    and not my first of the night!

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -4,20 +4,27 @@ class Array
   # this code is a bit of a fluke, it's got a test rather than a spec, see the test for
   # more info
   def intersection(arr2)
+
     self_sorted = self.sort {|a, b| a.to_s <=> b.to_s}
     target_sorted = arr2.sort {|a, b| a.to_s <=> b.to_s}
+
     intersection= []
+
     jstart=0
+
     for i in (0..self_sorted.length-1)
       for j in (jstart..target_sorted.length-1)
+
         if self_sorted[i] == target_sorted[j]
           jstart = j+1
           intersection[intersection.length] = self_sorted[i]
-          break
         end
+
       end
     end
+
     return intersection
+
   end
 
   # from lib/basic
